8162603e8fa87edb8aed4dc8da0d1262eb302e11
Carlos <cmoranmartinez@gmail.com>
2024-06-10 14:51:00 +0200

    Creating card with the data

--- src/controllers/drugController.js
+++ src/controllers/drugController.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const API_URL =
-  'https://api.fda.gov/drug/event.json?search=primarysourcecountry:"US"';
+  'https://api.fda.gov/drug/event.json?search=primarysourcecountry:"US"&limit=50';
 
 export const fetchDrugs = async () => {
   try {
     const responde = await axios.get(API_URL);
-    console.log(responde.data.results);
+    // console.log(responde.data.results);
+    return responde.data.results;
   } catch (err) {
     console.log("Error en el fetchDrugs");
     return [];
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,8 +1,15 @@
-import { Box, Button } from "@mui/material";
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { fetchDrugs } from "../controllers/drugController";
 
 export const Home = () => {
+  const [drug, setDrug] = useState([]);
+
+  const handleSearch = async () => {
+    const results = await fetchDrugs();
+    setDrug(results);
+    console.log("Esto es drug", results);
+  };
   return (
     <Box
       display="flex"
@@ -14,9 +21,31 @@ export const Home = () => {
       <h1 m={3}>Prueba técnica JARAXA pero en HOME</h1>
       <h3>Limpia y organización carpetas proyecto. Dependencias requeridas.</h3>
       <h3>Llamada API e interpretación respuesta.</h3>
-      <Button variant="contained" onClick={fetchDrugs}>
+      <Button variant="contained" onClick={handleSearch}>
         LLAMADA API
       </Button>
+      <Box mt={2}>
+        {drug?.length > 0 ? (
+          drug.map((drug, index) => (
+            <Box key={index} mb={2} p={2} border={1} borderRadius={4}>
+              <Typography variant="h6">
+                Company Number: {drug.companynumb}
+              </Typography>
+              <Typography variant="body1">
+                Safety Report ID: {drug.safetyreportid}
+              </Typography>
+              <Typography variant="body1">
+                Primary Source Country: {drug.primarysourcecountry}
+              </Typography>
+              <Typography variant="body1">
+                Seriousness: {drug.serious}
+              </Typography>
+            </Box>
+          ))
+        ) : (
+          <Typography variant="body1">No data available</Typography>
+        )}
+      </Box>
     </Box>
   );
 };
